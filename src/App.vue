f41cd2e3f1f824d3772e9a6032e8f656ab5e858c
Rajat Verma <rajat98v@gmail.com>
2020-10-23 20:55:20 +0530

    Added logic for followers

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="app">
+        @{{ user.username}} - {{ fullname}}
+        <strong>Followers </strong> {{ followers }}
+        <button @click="followUser">
+            Follow
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+                username: '_RajatVerma',
+                firstname: 'Rajat',
+                lastname: 'Verma',
+                email: 'rajatv89@gmail.com',
+                isAdmin: true,
+            }
+        }
+    },
+    watch: {
+        followers(newFollowerCount, oldFollowerCount) {
+            if (oldFollowerCount < newFollowerCount) {
+                console.log(`${ this.user.username } has gained a follower!`)
+            }
+        }
+    },
+    computed: {
+        fullname() {
+            return `${this.user.firstname} ${this.user.lastname}` // String Literals
+            // Eqivalent to this.user.firstname+" "+this.user.lastname
+        }
+    },
+    methods: {
+        followUser() {
+            this.followers++
+        }
+    },
+    mounted() {
+        this.followUser();
+    }
+
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+</style>
